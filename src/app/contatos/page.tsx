879486dc41980c91cdca09a0fee655ac21655790
giovaniflp <giovanifeitosa123@gmail.com>
2024-02-19 20:21:30 -0300

    Sobre nós e estilização básica

--- src/app/contatos/page.tsx
+++ src/app/contatos/page.tsx
@@ -6,26 +6,38 @@ export default function contatosPage(){
             <header className="bg-orange-400 p-4"><h1>Nossos Contatos!</h1></header>
             <nav className="my-4">
                 <ul className="flex flex-wrap gap-8 justify-center">
-                    <li className="bg-orange-600 rounded-lg p-2 w-60">
+                    <li className="bg-orange-600 rounded-lg p-2 w-72">
                         {/* Adicionar o componente Image do Next */}
                         <h5>Giovani Feitosa</h5>
                         <p><a className="hover:text-gray-600 duration-500" href="mailto:gflp@discente.ifpe.edu.br">Email - gflp@discente.ifpe.edu.br </a></p>
                     </li>
-                    <li className="bg-orange-600 rounded-lg p-2 w-60">
+                    <li className="bg-orange-600 rounded-lg p-2 w-72">
                         <h5>João Vitor</h5>
                         <p><a className="hover:text-gray-600 duration-500" href="mailto:jvl1@discente.ifpe.edu.br">Email - jvl1@discente.ifpe.edu.br </a></p>
                     </li>
-                    <li className="bg-orange-600 rounded-lg p-2 w-60">
+                    <li className="bg-orange-600 rounded-lg p-2 w-72">
                         <h5>Enio Batalha</h5>
                         <p><a className="hover:text-gray-600 duration-500" href="mailto:ehlb@discente.ifpe.edu.br">Email - ehlb@discente.ifpe.edu.br </a></p>
                     </li>
-                    <li className="bg-orange-600 rounded-lg p-2 w-60">
+                    <li className="bg-orange-600 rounded-lg p-2 w-72">
                         <h5>Ernani Reis</h5>
                         <p><a className="hover:text-gray-600 duration-500" href="mailto:esrn@discente.ifpe.edu.br">Email - esrn@discente.ifpe.edu.br </a></p>
                     </li>
-                    <li className="bg-orange-600 rounded-lg p-2 w-60">
+                    <li className="bg-orange-600 rounded-lg p-2 w-72">
                         <h5>Clauscyberion Emmanuel</h5>
                         <p><a className="hover:text-gray-600 duration-500" href="mailto:ceac1@discente.ifpe.edu.br">Email - ceac1@discente.ifpe.edu.br </a></p>
+                    </li>
+                    <li className="bg-orange-600 rounded-lg p-2 w-72">
+                        <h5>Jonathas Gabriel</h5>
+                        <p><a className="hover:text-gray-600 duration-500" href="mailto:jgsp@discente.ifpe.edu.br">Email - jgsp@discente.ifpe.edu.br </a></p>
+                    </li>
+                    <li className="bg-orange-600 rounded-lg p-2 w-72">
+                        <h5>Carlos Antônio</h5>
+                        <p><a className="hover:text-gray-600 duration-500" href="mailto:casj@discente.ifpe.edu.br">Email - casj@discente.ifpe.edu.br </a></p>
+                    </li>
+                    <li className="bg-orange-600 rounded-lg p-2 w-72">
+                        <h5>Matheus Victor</h5>
+                        <p><a className="hover:text-gray-600 duration-500" href="mailto:mvsc7@discente.ifpe.edu.br">Email - mvsc7@discente.ifpe.edu.br </a></p>
                     </li>
                 </ul>
             </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="bg-orange-400">
-      <header className="flex justify-between p-4">
-        <h1>Equipe 2</h1>
+    <div className="bg-white h-full">
+      <header className="flex justify-between items-center p-4 bg-orange-400">
+        <div className="flex items-center">
+          <Image className="absolute" alt="" src={"/icons/angryTomato.png"} width={60} height={60}></Image>
+          <h1 className="ml-16">MataFome</h1>
+        </div>
         <nav>
           <ul className="flex gap-2">
             <li>
-              <a href="">Sobre</a>
+              <a href="#sessaoSobre">Sobre</a>
             </li>
             <li>
               <a href="/contatos">Contato</a>
@@ -18,10 +24,51 @@ export default function Home() {
         </nav>
         </header>
         <main>
-          <section>
-            <h2></h2>
+          <section className="text-black">
+            <h2 className="text-black text-5xl p-4" id="sessaoSobre">Sobre nós!</h2>
+            <h3 className="px-4 text-xl">MataFome - Seu delivery de confiança</h3>
+            <section className="p-4">
+              <h3>Fome batendo? O MataFome te salva!</h3>
+              <p>Somos um delivery rápido e prático que oferece uma variedade de opções deliciosas para você matar a fome sem sair de casa.</p>
+              <ul>
+                  <li>Lanche: sanduíches, pizzas, salgados, pastéis e muito mais!</li>
+                  <li>Comida: pratos feitos, porções e acompanhamentos para todos os gostos.</li>
+                  <li>Bebidas: refrigerantes, sucos, água e outras opções para refrescar.</li>
+              </ul>
+            </section>
+            <section className="text-black p-4">
+              <h3 className="text-xl py-4">No MataFome você encontra:</h3>
+              <ul>
+                  <li>**Comida de qualidade:** Trabalhamos com ingredientes frescos e selecionados para garantir o melhor sabor para você.</li>
+                  <li>**Preços acessíveis:** Pratos com preços justos para que você possa se alimentar bem sem gastar muito.</li>
+                  <li>**Entrega rápida:** Entregamos seu pedido no conforto do seu lar em menos de 40 minutos*.</li>
+                  <li>**Atendimento diferenciado:** Nossa equipe é atenciosa e está sempre disposta a te ajudar.</li>
+              </ul>
+            </section>
+            <section className="flex justify-between bg-gray-500 text-white p-4">
+              <div className="flex gap-2">
+                <Link href={""}>
+                  <Image alt="" src={"/icons/gplay.svg"} width={40} height={40}></Image>
+                </Link>
+                <div>
+                  <h3>Peça já pelo nosso aplicativo</h3>
+                  <p>Acesse nosso app e faça seu pedido agora mesmo!</p>
+                </div>
+              </div>
+              <div>
+                <h3>Siga-nos nas redes sociais</h3>
+                <Link href={"https://www.instagram.com/matafomedeli/"} target="_blank">
+                  Instagram
+                </Link>
+              </div>
+            </section>
           </section>
         </main>
+        <footer className="bg-black flex flex-col items-center text-center p-4">
+          <p>MataFome: A sua fome, a nossa missão!</p>
+          <p className="text-xs">*Tempo de entrega pode variar de acordo com a sua localização.</p>
+          <p className="text-xs">**Tempo médio de entrega - 57 min.</p>
+        </footer>
     </div>
   )
 }
